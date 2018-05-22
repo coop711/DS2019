cde72a4eb7dec02d68dd5aca5f2aaf5f377374ba
coop711 <coop711@icloud.com>
2018-05-23 00:09:13 +0900

    Monty_Hall_v2.R added

--- R/Monty_Hall_v2.R
+++ R/Monty_Hall_v2.R
@@ -4,30 +4,25 @@ monty_hall <- function() {
   contestant <- sample(1:3, size = 1)
   monty <- ifelse(contestant == key, sample(goat, size = 1), setdiff(goat, contestant))
   switch <- setdiff(1:3, c(contestant, monty))
-  result <- ifelse(switch == key, 1, 0)
+  result <- ifelse(switch == key, "Switching wins", "Staying wins")
   # result
   c("Key" = key, "Contestant" = contestant, "Monty" = monty, "Switch" = switch, "Result" = result)
 }
 monty_hall()
-N <- 10000
+N <- 30
 monty_result <- replicate(N, monty_hall())
-# monty_result
-sum(monty_result[5, ])/N
-cumsum(monty_result[5, ])
-y <- cumsum(monty_result[5, ])
-y_win <- ifelse(monty_result[5, ] == 1, y, NA)
-y_win
-y_lose <- ifelse(monty_result[5, ] == 1, NA, y)
-y_lose
+monty_result[5, ]
+sum(monty_result[5, ] == "Switching wins")/N
+cumsum(monty_result[5, ] == "Switching wins")
+cumsum(monty_result[5, ] == "Staying wins")
+y_switch <- cumsum(monty_result[5, ] == "Switching wins")
+y_stay <- cumsum(monty_result[5, ] == "Staying wins")
 monty_plot <- function(N) {
   monty_result <- replicate(N, monty_hall())
-  y <- cumsum(monty_result[5, ])
-  y_win <- ifelse(monty_result[5, ] == 1, y, NA)
-  y_win
-  y_lose <- ifelse(monty_result[5, ] == 1, NA, y)
-  y_lose
+  y_switch <- cumsum(monty_result[5, ] == "Switching wins")
+  y_stay <- cumsum(monty_result[5, ] == "Staying wins")
 plot(x = 1:N, 
-     y = y_win / N, 
+     y = y_switch / N, 
      pch = 23, 
      col = "red", 
      bg = "red", 
@@ -38,23 +33,23 @@ plot(x = 1:N,
      cex = 0.7)
 axis(side = 2, 
      at = c(0, 1/3, 2/3, 3/4), 
-     labels = c("0", "1/3", "2/3", "3/4"))
+     labels = c("0", "1/3", "2/3", "3/4"), las = 2)
 points(x = 1:N, 
-       y = y_lose / N, 
+       y = y_stay / N, 
        pch = 22, 
        col = "blue", 
        bg = "blue", 
        cex = 0.7)
-abline(h = 2/3, lty = 3)
+abline(h = c(1/3, 2/3), lty = 3)
 legend("topleft", 
        inset = 0.05, 
-       legend = c("Win", "Lose"), 
+       legend = c("Switching wins", "Staying wins"), 
        pch = c(23, 22), 
        col = c("red", "blue"), 
        pt.bg = c("red", "blue"))
-sum(monty_result[5, ])/N
+sum(monty_result[5, ] == "Switching wins")/N
 }
 monty_plot(30)
-# monty_plot(100)
+monty_plot(100)
 table(unlist(monty_result[5, ]))
 
