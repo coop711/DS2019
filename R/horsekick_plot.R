bcbac1ca96cbf1c036e018f1250d7c9c1561d78c
Kee-Won Lee <kwlee@hallym.ac.kr>
2018-07-26 07:57:14 +0900

    ggplot code modified

--- R/horsekick_plot.R
+++ R/horsekick_plot.R
@@ -19,5 +19,5 @@ lines(c(3, 4), rep(horsekick_cum[4], 2), lty = 1, lwd = 2)
 lines(c(4, 5), rep(horsekick_cum[5], 2), lty = 1, lwd = 2)
 points(x = n_deaths, y = horsekick_cum,
        pch = 21, bg = "black", col = "black")
-points(x = n_deaths[2:5], y = horsekick_cum[1:4],
+points(x = tail(n_deaths, -1), y = head(horsekick_cum, -1),
        pch = 21, bg = "white", col = "black")
